a597ad6ebec056068b1b6126de2538fa266fcbea
pablo-johnson <pablo.88j@gmail.com>
2016-03-18 18:49:50 -0500

    Add Note screen

--- lesson2/app/src/main/java/com/studyjam/studyjam/lesson2/ui/NotesActivity.java
+++ lesson2/app/src/main/java/com/studyjam/studyjam/lesson2/ui/NotesActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -49,5 +52,34 @@ public class NotesActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        notesAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.newNote:
+                addNewNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void addNewNote() {
+        Intent intent = new Intent(this, AddNoteActivity.class);
+        startActivity(intent);
+    }
 
 }
