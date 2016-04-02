ae37ba7de328a988196c4892d3f6bb2cc07e8f7c
emedinaa <emedinaa@gmail.com>
2016-04-02 10:27:37 -0500

    *

--- Lesson3/app/src/main/java/com/project/jebus/sesson3/view/OnFragmentInteractionListener.java
+++
@@ -1,9 +0,0 @@
-package com.project.jebus.sesson3.view;
-
-/**
- * Created by jebus on 02/04/2016.
- */
-public interface OnFragmentInteractionListener {
-
-    void nextVersionAndroid(int position);
-}
