a39775348c551475b9d5f184614986c6c7bf92d2
sergio <sergio@onlinestudioproductions.com>
2016-04-02 08:47:24 -0500

    sesion 3

---
+++ Sesson3/app/src/main/java/com/project/jebus/sesson3/view/OnFragmentInteractionListener.java
@@ -0,0 +1,9 @@
+package com.project.jebus.sesson3.view;
+
+/**
+ * Created by jebus on 02/04/2016.
+ */
+public interface OnFragmentInteractionListener {
+
+    void nextVersionAndroid(int position);
+}
